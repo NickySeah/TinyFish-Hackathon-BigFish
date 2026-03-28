49bdb833ee01703347104c66e7c7ee4e24480366
Nicholas Seah <133653880+NickySeah@users.noreply.github.com>
2026-03-28 15:47:50 +0800

    Add additional allowed origins for CORS

--- backend/main.py
+++ backend/main.py
@@ -88,7 +88,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://tinyphish.vercel.app/", "tinyphish-git-main-nickyseahs-projects.vercel.app", "tinyphish-1pjc90dao-nickyseahs-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
